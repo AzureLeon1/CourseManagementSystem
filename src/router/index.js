9b8b392eb283ef67bc276f30f786aeef60e4e3ee
jianghua <tianfangjh@gmail.com>
2019-07-06 14:42:42 +0800

    初步添加课件列表

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Homepage from '@/views/Homepage'
 import Template from '@/views/Template'
+import Coursewarelist from '@/components/Coursewarelist'
 
 Vue.use(Router)
 
@@ -16,6 +17,12 @@ export default new Router({
       path: '/template',
       name: 'Template',
       component: Template
+    },
+    {
+      path: '/coursewarelist',
+      name: 'Coursewarelist',
+      component: Coursewarelist
+
     }
   ]
 })
