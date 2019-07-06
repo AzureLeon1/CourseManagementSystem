c3b92346fceea6c4453717ffbb82ac1fc27e0825
Avada Kedavra! <1499449418@qq.com>
2019-07-06 14:37:21 +0800

    创建MessageHome页面和MessageShortcut组件

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Homepage from '@/views/Homepage'
 import Template from '@/views/Template'
+import MessageHome from '@/views/MessageHome'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/template',
       name: 'Template',
       component: Template
+    },
+    {
+      path:'/MessageHome',
+      name:'MessageHome',
+      component: MessageHome
     }
   ]
 })
---
+++ src/views/MessageHome.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="MessageHome">
+        <div class="messageMenu">
+            <span style="display: inline-block; width: 60%; text-align: center;">消息</span>
+            <span style="display: inline-block; width: 20%; text-align: center;">来自</span>
+            <span style="display: inline-block; width: 20%; text-align: center;">时间</span>
+        </div>
+        <div class="messageWrapper">
+            part2-message short cut
+            <MessageShortcut />
+        </div>
+    </div>
+</template>
+
+<script>
+import MessageShortcut from '@/components/MessageShortcut';
+export default {
+    name: 'MessageHome',
+    components: {
+        MessageShortcut
+    }
+}
+</script>
+
+<style scoped>
+    *{
+        font-size: inherit;
+    }
+
+    /* MessageHome相当于page */
+    .MessageHome{
+        margin: 0 auto;
+        border-radius: 5px;
+        width: 60%;
+        
+        display: flex;
+        flex-direction: column;
+    }
+
+    .messageMenu{
+        height: 10px;
+        color: #8d8d8d;
+        font-size: 6px;
+        width: 100%;
+    }
+    
+    .messageWrapper{
+        width: 100%;
+        }
+</style>
