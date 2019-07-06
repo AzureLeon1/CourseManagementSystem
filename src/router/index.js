4827382b3ebce9c43966220c7f7e1da052e590cc
Leon <leonwangchn@163.com>
2019-07-06 10:56:01 +0800

    template views

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Homepage from '@/views/Homepage'
+import Template from '@/views/Template'
 
 Vue.use(Router)
 
@@ -9,7 +10,12 @@ export default new Router({
     {
       path: '/',
       name: 'Homepage',
-      component: Homepage
+      component: Homepage,
+    },
+    {
+      path: '/template',
+      name: 'Template',
+      component: Template
     }
   ]
 })
