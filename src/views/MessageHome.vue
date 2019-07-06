11000e8c2a54109cecf9c9caaf0cd12b5cf2b027
Chaos <chaoszh@foxmail.com>
2019-07-06 18:50:17 +0800

    messageHome for students initialized.

--- src/views/MessageHome.vue
+++ src/views/MessageHome.vue
@@ -1,15 +1,34 @@
 <template>
-    <div class="MessageHome">
-        <div class="messageMenu">
-            <span style="display: inline-block; width: 60%; text-align: center;">消息</span>
-            <span style="display: inline-block; width: 20%; text-align: center;">来自</span>
-            <span style="display: inline-block; width: 20%; text-align: center;">时间</span>
-        </div>
-        <div class="messageWrapper">
-            part2-message short cut
-            <MessageShortcut />
-        </div>
-    </div>
+    <el-container style="min-height: 500px; width: 80%; margin:20px auto 20px auto;">
+        <el-head></el-head>
+        <el-main style="padding: 15px; border: 1px solid #eee">
+            <el-table :data="tableData" >
+                <el-table-column prop="message" label="消息" min-width="200px;">
+                    <template slot-scope="scope">
+                        <p class="message">{{scope.row.message}}</p>
+                    </template>
+                </el-table-column>
+                <el-table-column prop="from" label="来自" width="150px">
+                    <template slot-scope="scope">
+                        <el-link class="from">{{scope.row.from}}</el-link>
+                    </template>
+                </el-table-column>
+                <el-table-column prop="time" label="时间" width="150px">
+                    <template slot-scope="scope">
+                        <i class="el-icon-time"></i>
+                        <span>{{scope.row.time}}</span>
+                    </template>
+                </el-table-column>
+            </el-table>
+        </el-main>
+        <el-footer style="text-align: center; margin: 10px auto;">
+            <el-pagination
+            layout="prev, pager, next"
+            :total="1000">
+            </el-pagination>
+        </el-footer>
+    </el-container>
+    
 </template>
 
 <script>
@@ -18,33 +37,27 @@ export default {
     name: 'MessageHome',
     components: {
         MessageShortcut
-    }
+    },
+    data() {
+        const simData={
+            message:"我是一条很长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长的广播",
+            from:"计算机系统结构",
+            time:"2019-6-19"
+        };
+        return {
+            tableData: Array(3).fill(simData)
+        }
+    },
 }
 </script>
 
 <style scoped>
-    *{
-        font-size: inherit;
-    }
+.message{
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 1;
+    text-overflow:ellipsis; 
+    overflow: hidden;
+}
 
-    /* MessageHome相当于page */
-    .MessageHome{
-        margin: 0 auto;
-        border-radius: 5px;
-        width: 60%;
-        
-        display: flex;
-        flex-direction: column;
-    }
-
-    .messageMenu{
-        height: 10px;
-        color: #8d8d8d;
-        font-size: 6px;
-        width: 100%;
-    }
-    
-    .messageWrapper{
-        width: 100%;
-        }
 </style>
