20aa59a5103d695621fbfcddf3c9a09e70db2947
ahazss <zhangjingmeng999@163.com>
2019-07-08 17:10:19 +0800

    update of design of Discussion

--- src/components/DiscussHome.vue
+++ src/components/DiscussHome.vue
@@ -1,49 +1,82 @@
 <template>
   <div id="discussHome">
     <div class="container">
-      <!--搜索框-->
 
       <el-collapse v-model="activeName" accordion>
-        <el-collapse-item class="question" title="问题讨论1" name="1">
-          <div class="comment">
-            回复回复
-            <el-button type="text" style="float:right;line-height:50px;">回复</el-button>
+        <el-collapse-item name="1">
+          <span class="collapse-title" slot="title">
+            {{quesDescription}}<span style="font-size:12px"><br>{{username}}发布于{{date}}</span>
+          </span>
+          <div v-for="num in commentNum" :key="num" v-show="num<showNum+1">
+            <div class="comment" style="display:flex;">
+              <div style="width:96%">{{username}}:{{respDescription}}
+                <div style="font-size:12px">{{date}}</div>
+              </div>
+              <el-button type="text" style="float:right;width:4%" @click="addCommentVisible = true">回复</el-button>
+            </div>
           </div>
-          <div class="comment">
-            评论评论
-             <el-button type="text" style="float:right;line-height:50px;">回复</el-button>
+          
+          <div class="comment-view" style="design:flex">
+            <el-button type="primary" icon="el-icon-more" @click="showMore">
+              <span>{{isShow?'查看全部':'收起评论'}}</span></el-button>
+            <el-button type="primary" icon="el-icon-plus" @click="addCommentVisible = true">添加评论</el-button>
           </div>
-          <div class="add-view" style="design:flex">
-            <el-button type="primary" icon="el-icon-more">查看更多</el-button>
-            <el-button type="primary" icon="el-icon-plus">添加评论</el-button>
+        </el-collapse-item>
+     
+        <el-collapse-item name="2">
+          <span class="collapse-title" slot="title">
+            {{quesDescription}}<span style="font-size:12px"><br>{{username}}发布于{{date}}</span>
+          </span>
+          <div v-for="num in commentNum" :key="num" v-show="num<showNum+1">
+            <div class="comment" style="display:flex;">
+              <div style="width:96%">{{username}}:{{respDescription}}
+                <div style="font-size:12px">{{date}}</div>
+              </div>
+              <el-button type="text" style="float:right;width:4%" @click="addCommentVisible = true">回复</el-button>
+            </div>
           </div>
+          <div class="comment-view" style="design:flex">
+            <el-button type="primary" icon="el-icon-more" @click="showMore">
+              <span>{{isShow?'查看全部':'收起评论'}}</span></el-button>
+            <el-button type="primary" icon="el-icon-plus" @click="addCommentVisible = true">添加评论</el-button>
+          </div>
+        </el-collapse-item>
+        
+        <el-collapse-item name="3">
+          <span class="collapse-title" slot="title">
+            {{quesDescription}}<span style="font-size:12px"><br>{{username}}发布于{{date}}</span>
+          </span>
+          <div v-for="num in commentNum" :key="num" v-show="num<showNum+1">
+            <div class="comment" style="display:flex;">
+              <div style="width:96%">{{username}}:{{respDescription}}
+                <div style="font-size:12px">{{date}}</div>
+              </div>
+              <el-button type="text" style="float:right;width:4%" @click="addCommentVisible = true">回复</el-button>
+            </div>
+          </div>
+          <div class="comment-view" style="design:flex">
+            <el-button type="primary" icon="el-icon-more" @click="showMore">
+              <span>{{isShow?'查看全部':'收起评论'}}</span></el-button>
+            <el-button type="primary" icon="el-icon-plus" @click="addCommentVisible = true">添加评论</el-button>
+          </div>
+        </el-collapse-item>
 
-        </el-collapse-item>
-        <el-collapse-item title="问题讨论2" name="2">
-          <div class="comment">评论
-             <el-button type="text" style="float:right;line-height:50px;">回复</el-button>
+        <el-collapse-item name="4">
+          <span class="collapse-title" slot="title">
+            {{quesDescription}}<span style="font-size:12px"><br>{{username}}发布于{{date}}</span>
+          </span>
+          <div v-for="num in commentNum" :key="num" v-show="num<showNum+1">
+            <div class="comment" style="display:flex;">
+              <div style="width:96%">{{username}}:{{respDescription}}
+                <div style="font-size:12px">{{date}}</div>
+              </div>
+              <el-button type="text" style="float:right;width:4%" @click="addCommentVisible = true">回复</el-button>
+            </div>
           </div>
-          <div class="comment">评论
-             <el-button type="text" style="float:right;line-height:50px;">回复</el-button>
-          </div>
-        </el-collapse-item>
-        <el-collapse-item title="问题讨论3" name="3">
-          <div class="comment">评论
-             <el-button type="text" style="float:right;line-height:50px;">回复</el-button>
-          </div>
-          <div class="comment">评论
-             <el-button type="text" style="float:right;line-height:50px;">回复</el-button>
-          </div>
-          <div class="comment">评论
-             <el-button type="text" style="float:right;line-height:50px;">回复</el-button>
-          </div>
-        </el-collapse-item>
-        <el-collapse-item title="问题讨论4" name="4">
-          <div class="comment">评论
-             <el-button type="text" style="float:right;line-height:50px;">回复</el-button>
-          </div>
-          <div class="comment">评论
-             <el-button type="text" style="float:right;line-height:50px;">回复</el-button>
+          <div class="comment-view" style="design:flex">
+            <el-button type="primary" icon="el-icon-more" @click="showMore">
+              <span>{{isShow?'查看全部':'收起评论'}}</span></el-button>
+            <el-button type="primary" icon="el-icon-plus" @click="addCommentVisible = true">添加评论</el-button>
           </div>
         </el-collapse-item>
       </el-collapse>
@@ -55,21 +88,37 @@
         </el-pagination>
       </div>
       <!-- Form 添加问题-->
-      <el-button type="primary" icon="el-icon-edit-outline" @click="dialogFormVisible = true"
+      <el-button type="primary" icon="el-icon-edit-outline" @click="addQuestionVisible = true"
                  style="font-size: 18px;">添加问题</el-button>
     </div>
       
-      <el-dialog title="添加问题" :visible.sync="dialogFormVisible">
+      <el-dialog title="添加问题" :visible.sync="addQuestionVisible">
         <el-form :model="form">
           <el-form-item label="问题描述" :label-width="formLabelWidth">
-            <el-input type="textarea" :rows="6" placeholder="请输入具体的问题描述" v-model="form.region"
-                      style="font-size: 15px;">
-            </el-input>
+            <el-input type="textarea" :rows="6" placeholder="请输入具体的问题描述"
+                      style="font-size: 15px;" maxlength="180" @input="descInput" v-model="desc">
+            </el-input><br>
+            <span class="text" style="float:right;color:#909399;margin-right:8px;">输入{{180-remnant}}/180字</span>
           </el-form-item>
         </el-form>
         <div slot="footer" class="dialog-footer">
-          <el-button @click="dialogFormVisible = false">取 消</el-button>
-          <el-button type="primary" @click="dialogFormVisible = false">完 成</el-button>
+          <el-button @click="addQuestionVisible = false">取 消</el-button>
+          <el-button type="primary" @click="addQuestionVisible = false">完 成</el-button>
+        </div>
+      </el-dialog>
+
+      <el-dialog title="添加评论" :visible.sync="addCommentVisible">
+        <el-form :model="form">
+          <el-form-item label="回复内容" :label-width="formLabelWidth">
+            <el-input type="textarea" :rows="6" placeholder="请输入具体的回复内容" 
+                      style="font-size: 15px;" maxlength="150" @input="descInput" v-model="desc">
+            </el-input><br>
+            <span class="text" style="float:right;color:#909399;margin-right:8px;">输入{{180-remnant}}/180字</span>
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="addCommentVisible = false">取 消</el-button>
+          <el-button type="primary" @click="addCommentVisible = false">完 成</el-button>
         </div>
       </el-dialog>
 
@@ -98,28 +147,39 @@ export default {
   data () {
     return {
       //activeName: '1'
-      dialogTableVisible: false,
-      dialogFormVisible: false,
-      form: {
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
-      },
+      addCommentVisible: false,
+      addQuestionVisible: false,
+      isShow:true,
+
+      username:'ABB',
+      date:'2019年07月07日',
+      quesDescription:'我是一段问题描述',
+      respDescription:'我是一段回复',
+      showNum:3,
+      commentNum:5,
+      remnant:180,
       formLabelWidth: '100px'
       }
   },
   methods: {
+    //倒计数
+    descInput(){
+      var txtVal=this.desc.length;
+      this.remnant=180-txtVal;
+    },
+
+    showMore(){
+      console.log('1',this.isShow);
+      this.isShow=!this.isShow;
+      this.showNum=this.isShow?4:this.commentNum;
+    }
 
   },
   mounted () {
 
   },
   computed: {
+   
   }
 }
 </script>
@@ -127,11 +187,17 @@ export default {
 <style>
 #discussHome
 .el-collapse-item__header{
+    flex: 1 0 auto;
+    order: -1;
     height:85px;   
-    font-size:16px;
+    font-size:14px;
     line-height:26px;
     text-align:left;
+}
 
+.collapse-title{
+  flex: 1 0 90%;
+  order: 1;
 }
 
 #discussHome
@@ -161,12 +227,12 @@ export default {
     padding-left:10px;
     padding-right:10px;
     font-size:14px;
-    line-height:70px;
+    line-height:24px;
     height:70px;
-    background-color:#E9EEF3
+    background-color:#ecf5ff
 }
 
-.add-view{
+.comment-view{
   margin-top:8px;
   justify-content: center;
   height:30px;
