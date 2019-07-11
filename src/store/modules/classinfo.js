6b5febdc9361858b7e8442e5fe24da0cfe14a79c
Leon <leonwangchn@163.com>
2019-07-11 15:54:53 +0800

    统一获取数据

--- src/store/modules/classinfo.js
+++ src/store/modules/classinfo.js
@@ -20,15 +20,16 @@ const actions = {
     {
       var data = {}
       console.log('角色到底是什么', role)
-    //   console.log('form到底是什么', form)
-    console.log(role);
-      if (role == 'student') {
-        data = await api.getClassInfo(form);
-        console.log('返回值到底是什么', data)
-      }
-      else if (role == 'teacher_edu') {
-        data = await api.getClassInfoForTea(form)
-      }
+
+      // if (role == 'student') {
+      //   data = await api.getClassInfo(form);
+      //   console.log('返回值到底是什么', data)
+      // }
+      // else if (role == 'teacher_edu') {
+      //   data = await api.getClassInfoForTea(form)
+      // }
+      // 统一获取数据的方式
+      data = await api.getClassInfo(form)
         console.log(data);
         localStorage.setItem("classinfo",JSON.stringify(data.data.data))
         // console.log(localStorage.getItem("classinfo"));
